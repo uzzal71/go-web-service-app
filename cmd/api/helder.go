165e86311a80c34f37719a3206bca46fc6815fa9
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-14 00:20:59 +0600

    helper

--- cmd/api/helder.go
+++ cmd/api/helder.go
@@ -6,5 +6,16 @@ import (
 )
 
 func (app *application) writeJSON(s http.ResponseWriter, status int, data any) error {
-	
+	js, err := json.MarshalIndent(data, "", \t"")
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
