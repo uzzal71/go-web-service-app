a491aaa20461ccfe04f147fcfc8b3cdaf8e49e51
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-14 00:22:13 +0600

    helper

--- cmd/api/handers.go
+++ cmd/api/handers.go
@@ -59,16 +59,7 @@ func (app *application) getCreateBooksHandler(w http.ResponseWriter, r *http.Req
 			},
 		}
 
-		js, err := json.MarshalIndent(books, "", "\t")
-		if err != nil{
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-
-		js = append(js, '\n')
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(js)
-		return
+		
 	}
 
 	if r.Method == http.MethodPost {
