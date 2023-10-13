7ee4f3b0d3ec9e595088cac9ebc337a454552b98
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-14 01:05:42 +0600

    json

--- cmd/api/handers.go
+++ cmd/api/handers.go
@@ -1,11 +1,16 @@
 package main 
 
+/*
+BODY='{"title": "The Black Soulston", "published":2001,"pages":107, "genres":["Fiction", "Mystery"], "rating":305}'
+url -i -d "$BODY" localhost:4000/v1/books
+*/
 import (
 	"fmt"
 	"net/http"
 	"strconv"
 	"encoding/json"
 	"time"
+	"io/ioutil"
 	"readinglist.uzzal.io/internal/data"
 )
 
@@ -67,12 +72,26 @@ func (app *application) getCreateBooksHandler(w http.ResponseWriter, r *http.Req
 
 	if r.Method == http.MethodPost {
 		var input struct {
-			Title string 		`json:"title"`
-			Published int 		`json:"published"`
-			Pages int 			`json:"pages"`	
-			Genres []string		`json:"genres"`
-			Rating float32		`json:"raring"`
+			Title    string   `json:"title"`
+			Published int      `json:"published"`
+			Pages    int      `json:"pages"`
+			Genres   []string `json:"genres"`
+			Rating   float32  `json:"rating"`
 		}
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		err = json.Unmarshal(body, &input)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(w, "%v\n", input)
 	}
 }
 
@@ -122,7 +141,59 @@ func (app *application) updateBook(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Bad Request", http.StatusBadRequest) // Use http.StatusBadRequest for a 400 Bad Request response
 		return
 	}
-	fmt.Fprintf(w, "Update the details of the book with ID: %d", idInt)
+	
+	var input struct {
+		Title    *string   `json:"title"`
+		Published *int      `json:"published"`
+		Pages    *int      `json:"pages"`
+		Genres   []string `json:"genres"`
+		Rating   *float32  `json:"rating"`
+	}
+
+	book := data.Book{
+		ID: idInt,
+		CreatedAt: time.Now(),
+		Title: "Echoes in the Darkness",
+		Published: 2019,
+		Pages: 300,
+		Genres: []string{"Fiction", "Thriller"},
+		Rating: 4.5,
+		Version: 1,
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	err = json.Unmarshal(body, &input)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	if input.Title != nil {
+		book.Title = *input.Title
+	}
+
+	if input.Published != nil {
+		book.Published = *input.Published
+	}
+
+	if input.Pages != nil {
+		book.Pages = *input.Pages
+	}
+
+	if len(input.Genres) > 0 {
+		book.Genres = *input.Genres
+	}
+
+	if input.Rating != nil {
+		book.Rating = *input.Rating
+	}
+
+	fmt.Fprintf(w, "%v\n", input)
 }
 
 func (app *application) deleteBook(w http.ResponseWriter, r *http.Request) {
