b9f848a2f70e21d036e22797600672b178988fa3
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-13 23:51:17 +0600

    test

--- internal/data/books.go
+++ internal/data/books.go
@@ -5,12 +5,12 @@ import (
 )
 
 type Book struct {
-	ID int64	`json:"id"`
+	ID int64			`json:"id"`
 	CreatedAt time.Time `json:"-"`
-	Title string 	`json:"title"`
-	Published int 	`json:"published,omitempty"`
-	Pages int 			`json:"published,omitempty"`	
-	Genres []string		`json:"published,omitempty"`
-	Rating float32		`json:"published,omitempty"`
-	Version int32		`json:"published,omitempty"`
+	Title string 		`json:"title"`
+	Published int 		`json:"published,omitempty"`
+	Pages int 			`json:"oages,omitempty"`	
+	Genres []string		`json:"genres,omitempty"`
+	Rating float32		`json:"raring,omitempty"`
+	Version int32		
 }
