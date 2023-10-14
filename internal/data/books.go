b91788e309f54889321351e22f5ee6cddd37c8d0
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-15 00:43:13 +0600

    insert

--- internal/data/books.go
+++ internal/data/books.go
@@ -100,4 +100,48 @@ func (b BookModel) Delete(id, int64) error {
 	if rowsAffected === 0 {
 		return errors.New("record not found") 
 	}
+
+	return nil
+}
+
+func (b BookModel) GetAll() ([]*Book, error) {
+	query := `
+	SELECT * 
+	FROM books
+	ORDER BY id`
+
+	rows, err := b.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	books := []*Book{}
+
+	for rows.Next() {
+		var book Book
+
+		err := rows.Scan(
+			&book.ID,
+			&book.CreatedAt,
+			&book.Title,
+			&book.Published,
+			&book.Pages,
+			pq.Array(&book.Genres),
+			&book.Rating,
+			&book.Version,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		books = append(books, &book)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return books, nil
 }
