8ac066321b8142d69aa008b2339496131374c5b9
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-14 22:23:08 +0600

    database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -30,6 +30,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|stage|prod)") // Use StringVar here
+	flag.StringVar(&cfg.dsn, "db-dsn", os.Getenv("READINGLIST_DB_DSN"), "PostgreSQL DSN")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
@@ -38,6 +39,19 @@ func main() {
 		config: cfg,
 		logger: logger,
 	}
+
+	db, err := sql.Open("postgres", cfg.dsn)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Pint()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	logger.Printf("database connection pool established")
 
 	addr := fmt.Sprintf(":%d", cfg.port)
 
