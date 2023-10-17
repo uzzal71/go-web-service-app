f41c0a2f19ecd8447148aabcd0a9a54ecc33e16f
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-17 22:20:39 +0600

    web

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,4 +12,13 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":80", "HTTP network address")
+	
+	srv := &http.Server{
+		Addr: *addr,
+		Handler: app.routes()
+	}
+
+	log.Printf("Staring the server on %s", *addr)
+	err := srv.ListenAndServe()
+	log.Fatal(err)
 }
