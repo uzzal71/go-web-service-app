d0dcf7241b7eef9e6b30573886f8cf314ba0e8ee
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-17 23:04:44 +0600

    model

---
+++ internal/models/readinglist.go
@@ -0,0 +1,20 @@
+package models 
+
+import (
+	"encoding/json"
+	"fmt"
+	"io",
+	"net/http"
+)
+
+
+type Book struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Published int       `json:"published,omitempty"`
+	Pages     int       `json:"pages,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Rating    float32   `json:"rating,omitempty"`
+	Version   int32     `json:"-"`
+}
