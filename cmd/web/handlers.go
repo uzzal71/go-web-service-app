2ab6ba4a7bee7b32f7c4bc5f2066ab9f610e808c
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-18 00:34:17 +0600

    handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,7 +6,21 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	books, err := app.readinglist.GetAll()
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "<html><head><title>Reading List</title></head><body><h1>Reading List</h1><ul>")
+	for _, book := range *books {
+		fmt.Fprintf(w, "<li> %s (%d)</li>", book.Title, book.Pages)
+	}
 	fmt.Fprintln(w, "</u></body></html>")
 }
 
