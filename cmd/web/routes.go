7ad43ba093117ea3d01af31549f4aa5ebad81f9f
uzzal71 <uzzalroy.acm@gmail.com>
2023-10-17 22:23:25 +0600

    web

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,4 +6,8 @@ func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/book/view", app.BookView)
+	mux.HandleFunc("/book/create", app.bookCreate)
+
+	return mux
 }
